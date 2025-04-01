8c80a9f6455fd00a21852394af3ab6250f452aa0
Felix Mayer <FelixM@yer.tools>
2025-04-01 09:26:37 +0200

    Add Info page component with features and values sections

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import DaisyUiIcon from "~icons/logos/daisyui-icon";
 import LucideIcon from "~icons/simple-icons/lucide";
 import ClockIcon from "~icons/tabler/clock";
 import CodeIcon from "~icons/tabler/code";
+import InfoCircleIcon from "~icons/tabler/info-circle";
 
 import { Component, ComponentProps, For, JSX, lazy } from "solid-js";
 
@@ -37,6 +38,11 @@ export const pageTree = {
     title: "Hello World!",
     component: lazy(() => import("./pages/Index")),
     icon: WorldIcon,
+  },
+  info: {
+    title: "Info",
+    component: lazy(() => import("./pages/Info")),
+    icon: InfoCircleIcon,
   },
   "work-clock": {
     title: "Work Clock",
